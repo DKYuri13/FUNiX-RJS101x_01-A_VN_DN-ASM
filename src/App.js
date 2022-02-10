2e43d327c1bf79f4290ebcb880b14ec6ce222cb7
Hoang Duy Quang <quang9514@gmail.com>
2022-02-10 18:35:07 +0700

    Third commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from "./components/StaffListComponent";
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { STAFFS, DEPARTMENTS, ROLE } from './shared/staffs';
+import React, { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      staffs: STAFFS
+    };
+  }
+
+  render() {
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -13,9 +24,10 @@ function App() {
           <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu staffs={this.state.staffs} />
     </div>
   );
+}
 }
 
 export default App;
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
-import { STAFFS } from '../shared/staffs';
+import { Card, CardImg, CardImgOverlay, CardBody,
+    CardTitle } from 'reactstrap';
 
 class Menu extends Component {
 
     constructor(props){
         super(props);
 
-        this.state = STAFFS
+        this.state = {
+            selectedStaff: null
+        }
+    }
+
+    onDishSelect(staff) {
+        this.setState({ selectedStaff: staff});
+    }
+
+    renderStaff(staff) {
+        if (staff != null)
+            return(
+                <Card>
+                    <CardImg top src={staff.name} alt={staff.name} />
+                    <CardBody>
+                      <CardTitle>{staff.name}</CardTitle>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
     }
 
     render() {
-        const staffList = this.state.STAFFS.map((staff) => {
+        const menu = this.props.staffs.map((staff)=>{
             return (
-                <div key={staff.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={staff.name} />
-                        </Media>
-                    </Media>
+                <div key={staff.id} className="col-12 col-md-5 m-1">
+                    <Card key={staff.id}
+                        onClick={() => this.onStaffSelect(staff)}>
+                        <CardImg width="100%" src={staff.image} alt={staff.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{staff.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             );
         });
@@ -26,9 +50,12 @@ class Menu extends Component {
         return (
             <div className='container'>
                 <div className='row'>
-                    <Media list>
-                        {staffList}
-                    </Media>
+                    {menu}
+                </div>
+                <div className="row">
+                  <div  className="col-12 col-md-5 m-1">
+                    {this.renderStaff(this.state.selectedStaff)}
+                  </div>
                 </div>
             </div>
         );
