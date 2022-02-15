4d9f98727fceac6e6afacacc1aa806bdc28356e3
Hoang Duy Quang <quang9514@gmail.com>
2022-02-15 12:39:43 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React, { Component } from "react";
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Menu from "./components/MenuComponents";
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state ={
-      dishes: DISHES
-    };
-  }
   
   render() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
